f297d03d1c952a38c07f388b57a5284acc2a0aee
Cami <cbrun.mine@gmail.com>
2022-09-29 12:40:57 -0600

    Created Chatbot class

--- src/chat/controller/Controller.java
+++ src/chat/controller/Controller.java
@@ -1,14 +1,26 @@
 package chat.controller;
 
+import chat.model.Chatbot;
+import java.util.Scanner;
+
 public class Controller
 {
+	private Chatbot chatbot;
+	private Scanner keyboardInput;
+	
+	
 	public Controller()
 	{
-		
+		this.chatbot = new Chatbot();
+		this.keyboardInput = new Scanner(System.in);
 	}
 	
 	public void start()
 	{
 		
+		
+		
+		
+		keyboardInput.close();
 	}
 }

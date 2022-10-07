d587fb281f9c2343d47e4169b74bc8dd953c8904
Cami <cbrun.mine@gmail.com>
2022-10-07 10:26:29 -0600

    Created Final Project

---
+++ SwiftFinal/CreateView.swift
@@ -0,0 +1,25 @@
+//
+//  CreateView.swift
+//  SwiftFinal
+//
+//  Created by Brunson, Cami on 10/7/22.
+//
+
+import SwiftUI
+
+struct CreateView: View
+{
+    var body: some View
+    {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
+        CreateView()
+    }
+}
---
+++ SwiftFinal/Resources/SwiftFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFinalApp.swift
+//  SwiftFinal
+//
+//  Created by Brunson, Cami on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CreateView()
+        }
+    }
+}

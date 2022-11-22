074ea6f8e057e38ba70bd8bbb69f05e7aa1bee46
Cami <cbrun.mine@gmail.com>
2022-11-22 13:38:49 -0700

    Created method to convert rgb to hsv

--- SwiftFinal/Model/RGBColor.swift
+++ SwiftFinal/Model/RGBColor.swift
@@ -37,4 +37,55 @@ class RGBColor
         
         return Color(red: redPercent, green: greenPercent, blue: bluePercent)
     }
+    
+    func convertToHSV() -> HSVColor
+    {
+        var hue : Double
+        var sat : Double
+        var value : Double
+        
+        let redPercent = Double(red) / 255.0
+        let greenPercent = Double(green) / 255.0
+        let bluePercent = Double(blue) / 255.0
+        
+        var max = max(redPercent, greenPercent, bluePercent)
+        var min = min(redPercent, greenPercent, bluePercent)
+        var delta = max - min
+        
+        //Hue Calculation
+        if (delta == 0)
+        {
+            hue = 0.0
+        }
+        else
+        {
+            switch max
+            {
+            case redPercent:
+                hue = (60.0 * ((greenPercent - redPercent) / delta).truncatingRemainder(dividingBy: 6))
+            case greenPercent:
+                hue = (60.0 * ((bluePercent - redPercent) / delta) + 2)
+            case bluePercent:
+                hue = (60.0 * ((redPercent - greenPercent) / delta) + 4)
+            default:
+                hue = 0.0
+            }
+        }
+        
+        //Sat Calculation
+        if (max != 0)
+        {
+            sat = (delta / max)
+        }
+        else
+        {
+            sat = 0.0
+        }
+        
+        //Value Calculation
+        value = max
+        
+        let finalColor : HSVColor = HSVColor(hue: hue, sat: sat, value: value)
+        return finalColor
+    }
 }
